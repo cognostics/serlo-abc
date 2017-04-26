176d1668d1836abafd681f37b62dd41ab19c78b8
Igor Zamyslov <igor.zamyslov@cognostics.de>
2017-04-26 15:16:36 +0200

    Production version (doesn’t use storybook component, but runs the application directly)

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-// @flow
-import App from './storybook';
-// import App from './src';
+import React from 'react';
+import loadFonts from './src/components/helpers/fonts';
+import { NativeModules } from 'react-native';
+import NativeApp from './src'
 
-export default App;
+const App = ({ fontsLoaded }) => fontsLoaded && <NativeApp />;
+
+export default loadFonts({
+  norddruck: require('./src/assets/fonts/norddruck.ttf'),
+  serlo: require('./src/assets/fonts/serlo.ttf')
+})(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,46 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Scene, Router, Actions } from 'react-native-router-flux';
+import { Button, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-const App = () => <Text>Welcome to Serlo ABC</Text>;
+import reducers from './reducers';
+import ExerciseApp from './containers/ExerciseApp';
+import Splash from './components/exercises/Splash';
+
+// router demonstration
+const reducer = combineReducers(reducers);
+const store = createStore(reducer);
+
+const BasicLayout = (props) => (
+  <View style={{ flex: 1 }}>
+    {props.children}
+  </View>
+)
+
+class RouterStory extends React.Component {
+  componentDidMount() {
+    setTimeout(function () {
+      Actions.exercise();
+    }, 3000);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Scene key="root" hideNavBar duration={0}>
+          <Scene key="splash" component={Splash} />
+          <Scene key="exercise" component={ExerciseApp} />
+        </Scene>
+      </Router>
+    );
+  }
+}
+
+const App = () => (
+  <Provider store={store}>
+    <RouterStory />
+  </Provider>
+);
 
 export default App;
